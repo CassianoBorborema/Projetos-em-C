b489b80f89f3d05161044009b6be2aa4fed96de2
CassianoBorborema <ladeiacassiano@gmail.com>
2025-09-01 01:14:00 -0300

    segunda atividade publicada

---
+++ Algoritmos 1/Atividades/Printfs.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("Bem vindo a disciplina SI100!\n");
+    printf("Escrevendo cada palavra em um printf:\n");
+
+    printf("Bem\n");
+    printf("Vindo\n");
+    printf("A\n");
+    printf("Disciplina\n");
+    printf("SI100!\n");
+
+    printf("Utilizando apenas uma linha agora por letra no mesmo printf:\n");
+    printf("B\ne\nm\n\nv\ni\nn\nd\no\n\na\n\nd\ni\ns\nc\ni\np\nl\ni\nn\na\n\nS\nI\n1\n0\n0\n!\n\n");
+    return 0;
+}
